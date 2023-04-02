703ffb993266623baf050a4daa8f8f16fe9a1bbd
Paco_Lorente <lo-rente@hotmail.com>
2023-04-02 23:12:48 +0200

    La entidad no se mueve en subida.2/4/23

--- Banco_de_pruebas.asm
+++ Banco_de_pruebas.asm
@@ -80,8 +80,8 @@ Indice_Sprite_izq defw Indice_Badsat_izq
 Puntero_DESPLZ_der defw 0
 Puntero_DESPLZ_izq defw 0
 
-Posicion_inicio defw $4721								; Dirección de pantalla donde aparece el objeto. [DRAW].
-Cuad_objeto db 1										; Almacena el cuadrante de pantalla donde se encuentra el objeto, (1,2,3,4). [DRAW]
+Posicion_inicio defw $50c1								; Dirección de pantalla donde aparece el objeto. [DRAW].
+Cuad_objeto db 3										; Almacena el cuadrante de pantalla donde se encuentra el objeto, (1,2,3,4). [DRAW]
 
 ; Variables de objeto. (Características).
 
@@ -132,7 +132,7 @@ Obj_dibujado db 0 										; Indica a [DRAW] si hay que PINTAR o BORRAR el obje
 
 ; Movimiento.
 
-Puntero_indice_mov defw Indice_mov_Abajo
+Puntero_indice_mov defw Indice_mov_Arriba
 Puntero_mov defw 0
 Contador_db_mov db 0
 Incrementa_puntero db 0
@@ -317,6 +317,7 @@ Frame
 
 2 push bc
 	call Mov_obj										; MOVEMOS y decrementamos (Numero_de_malotes)
+
 	ld a,(Ctrl_0)
 	bit 4,a
 	jr z,1F                                             ; Omitimos BORRAR/PINTAR si no hay movimiento.
@@ -387,6 +388,7 @@ Mov_obj
 	ret z
 
 	call Movimiento										; Desplazamos el objeto. MOVEMOS !!!!!
+
 	ld a,(Ctrl_0) 										; Salimos de la rutina SI NO HA HABIDO MOVIMIENTO !!!!!
 	bit 4,a
 	ret z
--- Genera_coordenadas.asm
+++ Genera_coordenadas.asm
@@ -1,6 +1,6 @@
 ; ------------------------------------------------------------------------
 ;
-;	28/03/23
+;	02/04/23
 ;
 ;	Proporciona las coordenadas del objeto a imprimir.
 ;	Fila superior "0", Columna izquierda "0".
@@ -16,18 +16,26 @@ Genera_coordenadas push bc
 	ld a,l
 	and $1f
 	ld (Coordenada_X),a 								; Coordenada Y del sprite, (0-$1f). Columnas.
+
 	call calcula_tercio
 	ld b,a 												; "0", "1" o "1" en función del tercio de pantalla.
+
 	ld e,0
 	ld a,l
 	and $e0
 	jr z,1F
 	inc b
-1 inc e
+
+1 inc b
+	dec b
+	jr z,2F												; Si estamos en el 1er tercio de pantalla y en la línea "0".
+;														; _ , salimos.
+3 inc e
 	sub 32
-	jr nz,1B
+	jr nz,3B
 	djnz 1B 
-	ld a,e
+
+2 ld a,e
 	ld (Coordenada_y),a
 
 	pop hl
