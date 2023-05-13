92575b94e62a323c9d0b88a3948c9460aa54ff65
Paco_Lorente <lo-rente@hotmail.com>
2023-05-13 10:30:50 +0200

    Creo Niveles.asm y renombro Cajas_y_disparos.asm

---
+++ Niveles.asm
@@ -0,0 +1,37 @@
+; 13/05/23
+
+Indice_de_niveles
+
+	defw Nivel_1
+	defw Nivel_2
+	defw Nivel_3
+	defw Nivel_4
+	defw Nivel_5
+
+;	...
+;	...
+;	+ Niveles ...
+
+	defw 0
+	defw 0
+
+Nivel_1 db 1,1,1,1,1 
+    db 1
+
+Nivel_2
+
+Nivel_3
+
+Nivel_4
+
+Nivel_5
+
+
+;---------------------------------------------------------------------------------------------------------------
+;
+;   13/5/23
+
+Inicia_puntero_indice_de_niveles ld hl,Indice_de_niveles
+    ld (Puntero_indice_NIVELES),hl
+    ret
+    
